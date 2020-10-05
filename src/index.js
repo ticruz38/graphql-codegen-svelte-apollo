575f1a57bf222a772e03a715ed49492a6c856ee1
Thibaut Duchêne <thib.duchene@gmail.com>
2020-10-05 11:49:25 +0200

    ts build

--- src/index.js
+++ src/index.js
@@ -5,16 +5,21 @@ var pascal_case_1 = require("pascal-case");
 var camel_case_1 = require("camel-case");
 module.exports = {
     plugin: function (schema, documents, config, info) {
+        var allAst = graphql_1.concatAST(documents.map(function (d) { return d.document; }));
+        var operations = allAst.definitions.filter(function (d) { return d.kind === graphql_1.Kind.OPERATION_DEFINITION; });
+        var operationImport = ("" + (operations.some(function (op) { return op.operation == "query"; }) ? "query, " : "") + (operations.some(function (op) { return op.operation == "mutation"; }) ? "mutation, " : "") + (operations.some(function (op) { return op.operation == "subscription"; })
+            ? "subscription, "
+            : "")).slice(0, -2);
         var imports = [
             config.clientPath
                 ? "import client from \"" + config.clientPath + "\";"
                 : "import { ApolloClient } from \"apollo-client\";",
-            "import {query, mutation, subscription} from \"svelte-apollo\";",
+            "import { " + operationImport + " } from \"svelte-apollo\";",
             "import { writable } from \"svelte/store\";",
+            "import gql from \"graphql-tag\"",
         ];
         // const docs = documents.filter(d => d.document.definitions.)
-        var allAst = graphql_1.concatAST(documents.map(function (d) { return d.document; }));
-        var operations = allAst.definitions.filter(function (d) { return d.kind === graphql_1.Kind.OPERATION_DEFINITION; })
+        var ops = operations
             .map(function (o) {
             var dsl = "export const " + o.name.value + "Doc = gql`" + documents.find(function (d) {
                 return d.rawSDL.includes(o.operation + " " + o.name.value);
@@ -22,19 +27,19 @@ module.exports = {
             var op = "" + o.name.value + pascal_case_1.pascalCase(o.operation);
             var opv = op + "Variables";
             // const doc = `const ${o.name.value}Doc = ${o}`
-            var operation = "export const " + o.name.value + " = (" + (config.clientPath ? "" : "client: ApolloClient, ") + "variables: " + opv + ") =>\n  " + o.operation + "<" + op + ", any, " + o.name.value + opv + ">(client, {\n    " + o.name.value + ": " + o.name.value + "Doc,\n    variables\n  })";
+            var operation = "export const " + o.name.value + " = (" + (config.clientPath ? "" : "client: ApolloClient, ") + "variables: " + opv + ") =>\n  " + o.operation + "<" + op + ", any, " + opv + ">(client, {\n    " + o.operation + ": " + o.name.value + "Doc,\n    variables\n  })";
             var statelessOperation = "";
             if (config.clientPath &&
                 !o.variableDefinitions.length &&
                 o.operation != "mutation") {
-                statelessOperation = "export const " + camel_case_1.camelCase(o.name.value) + " = writable<" + op + ">({}, (set) => {\n                      const p = " + o.name.value + "({})\n                      p.subscribe((v) => {\n                        v.then(res => {\n                          set(res.data || {})\n                        })\n                      })\n                    })";
+                statelessOperation = "export const " + camel_case_1.camelCase(o.name.value) + " = writable<" + op + ">({}, (set) => {\n                      const p = " + o.name.value + "({})\n                      p.subscribe((v) => {\n                        v[\"then\"](res => {\n                          set(res.data || {})\n                        })\n                      })\n                    })";
             }
             return dsl + "\n" + operation + "\n" + statelessOperation;
         })
             .join("\n");
         return {
             prepend: imports,
-            content: operations
+            content: ops
         };
     },
     validate: function (schema, documents, config, outputFile, allPlugins) {
